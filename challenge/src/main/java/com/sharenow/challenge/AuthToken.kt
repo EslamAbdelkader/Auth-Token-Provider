10e0f4d693812d6d39d94bfe5dfacb8d00c0ace2
Eslam Abdelkader <EslamAK11@gmail.com>
2019-08-01 16:53:39 +0200

    initial commit

---
+++ challenge/src/main/java/com/sharenow/challenge/AuthToken.kt
@@ -0,0 +1,29 @@
+package com.sharenow.challenge
+
+import org.threeten.bp.ZonedDateTime
+import org.threeten.bp.temporal.ChronoUnit
+
+/**
+ * DTO for an auth token.
+ *
+ * Please, do not change this file!
+ */
+data class AuthToken(
+    val token: String,
+    val validUntil: ZonedDateTime
+) {
+
+    /**
+     * Checks if this token has passed its validUntil date time.
+     */
+    fun isValid(currentTime: () -> ZonedDateTime): Boolean {
+        return millisUntilInvalid(currentTime) > 0
+    }
+
+    /**
+     * Calculates the time left for this token being valid in milliseconds.
+     */
+    fun millisUntilInvalid(currentTime: () -> ZonedDateTime): Long {
+        return currentTime().until(validUntil, ChronoUnit.MILLIS)
+    }
+}
